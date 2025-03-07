ab57564b21eed5a45bdf045f582ae643852ced13
manujbsharma <dr.dacron@gmail.com>
2025-03-07 13:50:14 +0530

    First Step in Angular

--- identity-management/src/app/app.component.ts
+++ identity-management/src/app/app.component.ts
@@ -1,12 +1,83 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { identity } from './identities';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    // RouterOutlet
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'identity-management';
+
+  // Identity:identity = {
+  //   firstName : "Manuj",
+  //   lastName : "Sharma",
+  //   accountName : "Facebook",
+  //   personalNumber : 1002345,
+  //   costCenter : "Delhi",
+  //   department : "IT",
+  //   startDate : new Date('1977-09-24'),
+  //   endDate : new Date, 
+
+  // }
+
+  Identities:identity[] = [{
+    firstName : "Manuj",
+    lastName : "Sharma",
+    accountName : "Facebook",
+    personalNumber : 1002345,
+    costCenter : "Delhi",
+    department : "IT",
+    startDate : new Date('1977-09-24'),
+    endDate : new Date, 
+
+  },{
+    firstName : "Manuj",
+    lastName : "Sharma",
+    accountName : "Facebook",
+    personalNumber : 1002345,
+    costCenter : "Delhi",
+    department : "IT",
+    startDate : new Date('1977-09-24'),
+    endDate : new Date, 
+
+  },{
+    firstName : "Manuj",
+    lastName : "Sharma",
+    accountName : "Facebook",
+    personalNumber : 1002345,
+    costCenter : "Delhi",
+    department : "IT",
+    startDate : new Date('1977-09-24'),
+    endDate : new Date, 
+
+  },{
+    firstName : "Manuj",
+    lastName : "Sharma",
+    accountName : "Facebook",
+    personalNumber : 1002345,
+    costCenter : "Delhi",
+    department : "IT",
+    startDate : new Date('1977-09-24'),
+    endDate : new Date, 
+
+  },{
+    firstName : "Manuj",
+    lastName : "Sharma",
+    accountName : "Facebook",
+    personalNumber : 1002345,
+    costCenter : "Delhi",
+    department : "IT",
+    startDate : new Date('1977-09-24'),
+    endDate : new Date('2005-09-23'), 
+
+  },
+]
 }
---
+++ identity-management/src/app/identities.ts
@@ -0,0 +1,10 @@
+export interface identity{
+    firstName: string,
+    lastName:string,
+    accountName: string,
+    personalNumber: number,
+    costCenter: string,
+    department: string,
+    startDate: Date,
+    endDate: Date,    
+}
