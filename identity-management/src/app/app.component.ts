0b073b66a1300b2323a888503d826d1919f8e6b3
manujbsharma <dr.dacron@gmail.com>
2025-03-10 13:35:13 +0530

    Inputform for identities created  (layout)

--- identity-management/src/app/app.component.ts
+++ identity-management/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { identity } from './identities';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   imports: [
     // RouterOutlet
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -28,56 +30,60 @@ export class AppComponent {
 
   // }
 
-  Identities:identity[] = [{
-    firstName : "Manuj",
-    lastName : "Sharma",
-    accountName : "Facebook",
-    personalNumber : 1002345,
-    costCenter : "Delhi",
-    department : "IT",
-    startDate : new Date('1977-09-24'),
-    endDate : new Date, 
+  Identities: identity[] = [{
+    firstName: "Manuj",
+    lastName: "Sharma",
+    accountName: "Facebook",
+    personalNumber: 1002345,
+    costCenter: "Delhi",
+    department: "IT",
+    startDate: new Date('1977-09-24'),
+    endDate: new Date,
 
-  },{
-    firstName : "Manuj",
-    lastName : "Sharma",
-    accountName : "Facebook",
-    personalNumber : 1002345,
-    costCenter : "Delhi",
-    department : "IT",
-    startDate : new Date('1977-09-24'),
-    endDate : new Date, 
+  }, {
+    firstName: "Manuj",
+    lastName: "Sharma",
+    accountName: "Facebook",
+    personalNumber: 1002345,
+    costCenter: "Delhi",
+    department: "IT",
+    startDate: new Date('1977-09-24'),
+    endDate: new Date,
 
-  },{
-    firstName : "Manuj",
-    lastName : "Sharma",
-    accountName : "Facebook",
-    personalNumber : 1002345,
-    costCenter : "Delhi",
-    department : "IT",
-    startDate : new Date('1977-09-24'),
-    endDate : new Date, 
+  }, {
+    firstName: "Manuj",
+    lastName: "Sharma",
+    accountName: "Facebook",
+    personalNumber: 1002345,
+    costCenter: "Delhi",
+    department: "IT",
+    startDate: new Date('1977-09-24'),
+    endDate: new Date,
 
-  },{
-    firstName : "Manuj",
-    lastName : "Sharma",
-    accountName : "Facebook",
-    personalNumber : 1002345,
-    costCenter : "Delhi",
-    department : "IT",
-    startDate : new Date('1977-09-24'),
-    endDate : new Date, 
+  }, {
+    firstName: "Manuj",
+    lastName: "Sharma",
+    accountName: "Facebook",
+    personalNumber: 1002345,
+    costCenter: "Delhi",
+    department: "IT",
+    startDate: new Date('1977-09-24'),
+    endDate: new Date,
 
-  },{
-    firstName : "Manuj",
-    lastName : "Sharma",
-    accountName : "Facebook",
-    personalNumber : 1002345,
-    costCenter : "Delhi",
-    department : "IT",
-    startDate : new Date('1977-09-24'),
-    endDate : new Date('2005-09-23'), 
+  }, {
+    firstName: "Manuj",
+    lastName: "Sharma",
+    accountName: "Facebook",
+    personalNumber: 1002345,
+    costCenter: "Delhi",
+    department: "IT",
+    startDate: new Date('1977-09-24'),
+    endDate: new Date('2005-09-23'),
 
-  },
-]
+  },];
+
+  DeleteIdentity(MyIdentity: identity): void {
+    // filter the identities(all records), just remove the current records
+    this.Identities = this.Identities.filter(i => i != MyIdentity) // Take my set of identites (identities), put filter on it, and in filter => take all (i) beside/except the one i was entering/selecting (MyIdentity)
+  }
 }
